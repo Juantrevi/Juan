f17544f6e7d178c48cdb9c1d00ceb333bfd65756
Juantrevi <juantrevi70@gmail.com>
2024-02-08 16:17:50 +0000

    message

--- app/src/main/java/com/stu71563/thursday1/MainActivity.kt
+++ app/src/main/java/com/stu71563/thursday1/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Thursday1Theme {
                 // A surface container using the 'background' color from the theme
+                //
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
